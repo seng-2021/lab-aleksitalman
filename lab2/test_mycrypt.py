eba2281882f51a699d6c96d8922e8136e6c837e5
aleksitalman <115454517+aleksitalman@users.noreply.github.com>
2023-02-18 13:42:42 +0200

    trying values from negative to positive

--- lab2/test_mycrypt.py
+++ lab2/test_mycrypt.py
@@ -42,7 +42,7 @@ def test_invalid_char(invalid_input):
         mycrypt.encode(invalid_input)
 
 
-@pytest.mark.parametrize("invalid_input", [1, 2.2])
+@pytest.mark.parametrize("invalid_input", [-1, 5])
 def test_invalid_types(invalid_input):
     '''Invalid parameter types should raise TypeError'''
     with pytest.raises(TypeError):
