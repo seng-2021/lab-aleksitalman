f79fcfb98ad789b5e4d9ca80ec8cbcafb412b48a
aleksitalman <115454517+aleksitalman@users.noreply.github.com>
2023-02-18 13:01:07 +0200

    fixed bugs 1-3

--- lab2/mycrypt.py
+++ lab2/mycrypt.py
@@ -8,17 +8,28 @@ def encode(s):
     digitmapping = dict(zip('1234567890!"#€%&/()=','!"#€%&/()=1234567890'))
     if len(s) > 1000:
         raise ValueError
+    #max value
+    s = s.ljust(1000,"x")
     for c in s:
+        #invalid characters (not UTF-8)
+        if c in ["ä", "ö", "å"]:
+            raise ValueError
         if c.isalpha():
             if c.islower():
                 c=c.upper()
+            #if not lower case then upper case
+            else:
+                c.lower()
             # Rot13 the character for maximum security
             crypted+=codecs.encode(c,'rot13')
         elif c in digitmapping:
           crypted+=digitmapping[c]
-
-    return crypted
+        #if not letter and not in digitmapping then it is wrong
+        else:
+            raise ValueError
+    #return the original length
+    return crypted[:origlen]
 
 def decode(s):
-    return s
+    return encode(s)
 
